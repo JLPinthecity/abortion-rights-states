517a717ab7cf9161fdb87c182586917f2aa9cb1a
Joann Pan <joannlpan@gmail.com>
2022-07-17 19:00:05 -0400

    import react usa map package and add path css to globals

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,43 @@
+import React, { Component } from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import USAMap from "react-usa-map";
 
-const Home = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>What Are My Abortion Rights?</title>
-      </Head>
-      <h1>Welcome to Next.js!</h1>
-    </div>
-  );
-};
+class Home extends Component {
+  /* mandatory */
+  mapHandler = (event) => {
+    alert(event.target.dataset.name);
+  };
+
+  statesCustomConfig = () => {
+    return {
+      NJ: {
+        fill: "navy",
+        clickHandler: (event) =>
+          console.log("Custom handler for NJ", event.target.dataset),
+      },
+      NY: {
+        fill: "#CC0000",
+      },
+    };
+  };
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <Head>
+          <title>What Are My Abortion Rights?</title>
+        </Head>
+
+        <div className="App">
+          <USAMap
+            customize={this.statesCustomConfig}
+            onClick={this.mapHandler}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Home;
