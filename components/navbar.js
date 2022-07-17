bd89f7b0bc87cb581f9873ab10bf9672eb1aab5d
Joann Pan <joannlpan@gmail.com>
2022-07-17 16:26:38 -0400

    clean up layout of tutorial material and import navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,20 +1,23 @@
 import Link from "next/link";
+import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
   return (
     <div>
-      <div>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
+      <div className={styles.navbar}>
+        <div className={styles.links}>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
 
-        <Link href="/about">
-          <a>About</a>
-        </Link>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
 
-        <Link href="/contact">
-          <a>Contact</a>
-        </Link>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </div>
       </div>
     </div>
   );
