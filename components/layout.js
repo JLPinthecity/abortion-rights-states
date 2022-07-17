6539011b929a1ef0e395ef7e746f47f53480681a
Joann Pan <joannlpan@gmail.com>
2022-07-17 16:35:26 -0400

    download react svg map package

--- components/layout.js
+++ components/layout.js
@@ -1,19 +1,22 @@
 import Head from "next/head";
-import Link from "next/link";
 import styles from "../styles/Layout.module.css";
 import Navbar from "./navbar";
 
 const Layout = ({ children }) => {
   return (
-    <div className={styles.layout}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="Abortion laws by state" />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
+    <>
       <Navbar />
-    </div>
+      <div>{children}</div>
+    </>
+    // <div className={styles.layout}>
+    //   <Head>
+    //     <link rel="icon" href="/favicon.ico" />
+    //     <meta name="description" content="Abortion laws by state" />
+    //     <meta name="og:title" content={siteTitle} />
+    //     <meta name="twitter:card" content="summary_large_image" />
+    //   </Head>
+    //   <Navbar />
+    // </div>
   );
 };
 
