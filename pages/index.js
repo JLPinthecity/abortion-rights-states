3fd2eb9781afd43761e35f046ea6d28580af2736
Joann Pan <joannlpan@gmail.com>
2022-07-19 19:08:30 -0400

    change home to functional component to fix errors

--- pages/index.js
+++ pages/index.js
@@ -1,101 +1,53 @@
-import React, { Component } from "react";
+/* eslint-disable react/display-name */
+import React, { Component, useState } from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import states from "../constants/states";
 import USAMap from "react-usa-map";
 
-class Home extends Component {
+export default function Home(props) {
   /* mandatory */
-  mapHandler = (event) => {
-    alert(event.target.dataset.name);
+
+  /* 
+RTP:
+Changed class component to functional component. 
+Eliminated class logic. 
+Put states in constant file 
+
+*/
+  const getStateFullName = (stateAbbr) => {
+    console.log(stateAbbr);
+    console.log("States list", states);
+    return states[`${stateAbbr}`];
+  };
+
+  const mapHandler = (event) => {
+    console.log("Map handler works!", event.target.dataset.name);
+    getStateFullName(event.target.dataset.name);
   };
 
   //clicking on map returns state abbrev
-  states = {
-    AZ: "Arizona",
-    AL: "Alabama",
-    AK: "Alaska",
-    AR: "Arkansas",
-    CA: "California",
-    CO: "Colorado",
-    CT: "Connecticut",
-    DE: "Delaware",
-    FL: "Florida",
-    GA: "Georgia",
-    HI: "Hawaii",
-    ID: "Idaho",
-    IL: "Illinois",
-    IN: "Indiana",
-    IA: "Iowa",
-    KS: "Kansas",
-    KY: "Kentucky",
-    LA: "Louisiana",
-    ME: "Maine",
-    MD: "Maryland",
-    MA: "Massachusetts",
-    MI: "Michigan",
-    MN: "Minnesota",
-    MS: "Mississippi",
-    MO: "Missouri",
-    MT: "Montana",
-    NE: "Nebraska",
-    NV: "Nevada",
-    NH: "New Hampshire",
-    NJ: "New Jersey",
-    NM: "New Mexico",
-    NY: "New York",
-    NC: "North Carolina",
-    ND: "North Dakota",
-    OH: "Ohio",
-    OK: "Oklahoma",
-    OR: "Oregon",
-    PA: "Pennsylvania",
-    RI: "Rhode Island",
-    SC: "South Carolina",
-    SD: "South Dakota",
-    TN: "Tennessee",
-    TX: "Texas",
-    UT: "Utah",
-    VT: "Vermont",
-    VA: "Virginia",
-    WA: "Washington",
-    WV: "West Virginia",
-    WI: "Wisconsin",
-    WY: "Wyoming",
+
+  const statesCustomConfig = {
+    NJ: {
+      fill: "navy",
+      clickHandler: (event) =>
+        console.log("Custom handler for NJ", event.target.dataset),
+    },
+    NY: {
+      fill: "#CC0000",
+    },
   };
 
-  getStateFullName = (stateAbbr) => {
-    return states[stateAbbr];
-  };
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>What Are My Abortion Rights?</title>
+      </Head>
 
-  statesCustomConfig = () => {
-    return {
-      NJ: {
-        fill: "navy",
-        clickHandler: (event) =>
-          console.log("Custom handler for NJ", event.target.dataset),
-      },
-      NY: {
-        fill: "#CC0000",
-      },
-    };
-  };
-
-  render() {
-    return (
-      <div className={styles.container}>
-        <Head>
-          <title>What Are My Abortion Rights?</title>
-        </Head>
-
-        <div className="App">
-          <USAMap
-            customize={this.statesCustomConfig}
-            onClick={this.mapHandler}
-          />
-        </div>
+      <div className="App">
+        <USAMap customize={statesCustomConfig} onClick={mapHandler} />
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default Home;
