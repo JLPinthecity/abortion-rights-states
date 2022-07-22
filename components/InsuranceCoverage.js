b4b3572ed38ab1a057cc799be16f2f98c073869f
Joann Pan <joannlpan@gmail.com>
2022-07-21 20:23:50 -0400

    add requires coverage insurance data

--- components/InsuranceCoverage.js
+++ components/InsuranceCoverage.js
@@ -1,8 +1,32 @@
 const InsuranceCoverage = (props) => {
-  debugger;
+  const state = props.state;
+
+  const targetData = props.stats[state];
+
+  // debugger;
+
+  const requiresCoverage = targetData.requires_coverage
+    ? targetData.requires_coverage
+    : null;
+
   return (
     <div>
-      <div>Insurance Coverage TEST TEST TEST </div>
+      <div>
+        <b>
+          Insurance Coverage (abortion restrictions that specifically restrict
+          what type of insurance coverage can be used to cover abortion (both
+          Medicaid and private insurance))
+        </b>
+      </div>
+
+      <div>
+        <b>Requires coverage:</b>
+      </div>
+      <div>
+        {requiresCoverage
+          ? "If checked, the state requires that require private health plans regulated by the state cover abortion. These requirements do not apply to self-insured plans (in which the employer takes on all the risk, instead of contracting with a health insurer) as self-insured plans are regulated at the federal, not state, level."
+          : "This state does not have require coverage."}
+      </div>
     </div>
   );
 };
