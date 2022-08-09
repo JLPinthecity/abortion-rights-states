bf5a4336f73f06d24e18696a917a1090de111f28
Joann Pan <joannlpan@gmail.com>
2022-08-09 17:09:22 -0400

    complete adding data from insurance coverage table

--- components/InsuranceCoverage.js
+++ components/InsuranceCoverage.js
@@ -105,6 +105,37 @@ const InsuranceCoverage = (props) => {
       ? targetData.medicaid_coverage_provider_patient_decision
       : null;
 
+  const medicaid_exception_life = targetData.medicaid_exception_life
+    ? targetData.medicaid_exception_life
+    : null;
+
+  const medicaid_exception_health = targetData.medicaid_exception_health;
+
+  const medicaidExceptionHealth = () => {
+    switch (medicaid_exception_health) {
+      case "physical":
+        return "This state allows Medicaid  coverage of abortion when the pregnant person's physical health is endangered. (This is a requirement of the Hyde amendment.)";
+      default:
+        return "No exceptions for serious health conditions specified.";
+    }
+  };
+
+  const medicaid_exception_fetal = targetData.medicaid_exception_fetal;
+
+  const medicaidExceptionFetal = () => {
+    switch (medicaid_exception_fetal) {
+      case "Serious fetal anomaly":
+        return "This state allows Medicaid  coverage of abortion when the fetus has a serious or lethal fetal anomaly.";
+      default:
+        return "No exception for fetal anomaly.";
+    }
+  };
+
+  const medicaid_exception_rape_or_incest =
+    targetData.medicaid_exception_rape_or_incest
+      ? targetData.medicaid_exception_rape_or_incest
+      : null;
+
   return (
     <div className={styles.data_wrapper}>
       <div className={styles.insurance_type}>
@@ -237,6 +268,32 @@ const InsuranceCoverage = (props) => {
         {medicaid_coverage_provider_patient_decision
           ? "Yes, this state has a policy in place to use Medicaid funds to pay for abortion."
           : "No, this state does not have a policy in place for Medicaid funds to cover abortion."}
+      </div>
+
+      <h3 className={styles.secondary_label}>
+        <i>EXCEPTIONS TO MEDICAID ABORTION-COVERAGE RESTRICTIONS:</i>
+      </h3>
+
+      <div className={styles.label}>Life-saving exception:</div>
+
+      <div className={styles.answer}>
+        {medicaid_exception_life
+          ? "The state allows Medicaid insurance to cover abortion when it's necessary to save the pregnant person's life. (This is required by the Hyde amendment.)"
+          : "No life-saving exemption"}
+      </div>
+
+      <div className={styles.label}>Physical health exception:</div>
+      <div className={styles.answer}>{medicaidExceptionHealth()}</div>
+
+      <div className={styles.label}>SERIOUS FETAL ANOMALY EXCEPTION:</div>
+
+      <div className={styles.answer}>{medicaidExceptionFetal()}</div>
+
+      <div className={styles.label}>RAPE OR INCEST Exception:</div>
+      <div className={styles.answer}>
+        {medicaid_exception_rape_or_incest
+          ? "This state allows Medicaid insurance coverage for cases where pregnancy is a result of rape or incest. (This is required by the Hyde amendment)."
+          : "No rape or incest exception."}
       </div>
     </div>
   );
