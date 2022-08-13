aa3c04ba54009d229e45dce97deb61bc27b29aa4
Joann Pan <joannlpan@gmail.com>
2022-08-13 19:58:16 -0400

    separate out private insurance data into own component

--- components/InsuranceCoverage.js
+++ components/InsuranceCoverage.js
@@ -1,5 +1,6 @@
 import styles from "../styles/Section.module.css";
 import React, { useState } from "react";
+import PrivateInsurance from "./PrivateInsurance";
 
 const InsuranceCoverage = (props) => {
   const state = props.state;
@@ -16,50 +17,6 @@ const InsuranceCoverage = (props) => {
     if (showMore) return styles.showContent;
     else return styles.hideContent;
   };
-
-  // PRIVATE PLANS
-  const requiresCoverage = targetData.requires_coverage
-    ? targetData.requires_coverage
-    : null;
-
-  const privateCoverage = targetData.private_coverage_no_restrictions
-    ? targetData.private_coverage_no_restrictions
-    : null;
-
-  const privateLifeException = targetData.private_exception_life
-    ? targetData.private_exception_life
-    : null;
-
-  const private_exception_fetal = targetData.private_exception_fetal
-    ? targetData.private_exception_fetal
-    : null;
-
-  const private_exception_health = targetData.private_exception_health
-    ? targetData.exception_health
-    : null;
-
-  const privateHealthException = () => {
-    switch (private_exception_health) {
-      case "major bodily function":
-        return "This state allows private insurance coverage when the pregnant person's faces a 'substantial and irreversible impairment of a major bodily function.'";
-      default:
-        return "No exceptions for serious health conditions specified.";
-    }
-  };
-
-  const privateFetalException = () => {
-    switch (private_exception_fetal) {
-      case "Lethal fetal anomaly":
-        return "This state allows private insurance coverage when the fetus has a lethal fetal anomaly.";
-      default:
-        return "No exceptions for lethal fetal anomalies specified.";
-    }
-  };
-
-  const private_exception_rape_or_incest =
-    targetData.private_exception_rape_or_incest
-      ? targetData.private_exception_rape_or_incest
-      : null;
 
   // EXCHANGE PLANS
   const exchange_exception_health = targetData.exchange_exception_health
@@ -152,82 +109,21 @@ const InsuranceCoverage = (props) => {
       ? targetData.medicaid_exception_rape_or_incest
       : null;
 
+  const onToggle = () => {
+    return true ? true : false;
+  };
+
   return (
     <div className={styles.data_wrapper}>
+      <PrivateInsurance state={state} targetData={targetData} />
+
       <section className={styles.accordian}>
-        <div className={styles.insurance_type}>
-          Private health insurance plans
-        </div>
-        {/* IF SHOWMORE IS TRUE, SHOW -, 
-        IF SHOWMORE IS FALSE(DEFAULT), SHOW + */}
-        <span onClick={() => toggle()}>{showMore ? "-" : "+"}</span>
-
-        <div className={getStyle()}>
-          <div className={styles.label}>
-            Does the state require abortion coverage by private health plans
-            regulated by state?
-          </div>
-
-          <div className={styles.answer}>
-            {requiresCoverage
-              ? "Yes, private health plans regulated by the state are required to cover abortions. This regulation does not apply to self-insured plans regulated at the federal versus the state level)."
-              : "No, this state does not have require abortion coverage by private health plans regulated by state."}
-          </div>
-
-          <div className={styles.label}>
-            <b>
-              Can private insurance cover abortions despite not being required
-              to?
-            </b>
-          </div>
-          <div className={styles.answer}>
-            {privateCoverage
-              ? "Private insurance may cover abortion but not necessarily required to."
-              : "This state does not have require coverage and private insurance cannot cover abortion."}
-          </div>
-
-          <div className={styles.exceptions}>
-            <h3 className={styles.secondary_label}>
-              <i>
-                EXCEPTIONS TO PRIVATE HEALTH INSURANCE ABORTION-COVERAGE
-                RESTRICTIONS:
-              </i>
-            </h3>
-
-            <div className={styles.label}>Life-saving exception:</div>
-
-            <div className={styles.answer}>
-              {privateLifeException
-                ? "This state allows private insurance coverage for cases where abortion is necessary to save the pregnant person's life."
-                : "No life-saving exemption"}
-            </div>
-
-            <div className={styles.label}>
-              Serious health condition exception:
-            </div>
-
-            <div className={styles.answer}>{privateHealthException()}</div>
-
-            <div className={styles.label}>Lethal fetal anomaly Exception:</div>
-
-            <div className={styles.answer}>{privateFetalException()}</div>
-
-            <div className={styles.label}>RAPE OR INCEST Exception:</div>
-            <div className={styles.answer}>
-              {private_exception_rape_or_incest
-                ? "This state allows private insurance coverage for cases where pregnancy is a result of rape or incest."
-                : "No rape or incest exception."}
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section className={styles.marketplace}>
         <div className={styles.insurance_type}>
           State marketplace insurance plans
         </div>
+        <span onClick={() => toggle()}>{showMore ? "-" : "+"}</span>
 
-        <div className={styles.subsection}>
+        <div className={getStyle()}>
           <div className={styles.label}>
             Does this state prohibit any and all exchange insurance coverage for
             abortion?
@@ -288,49 +184,56 @@ const InsuranceCoverage = (props) => {
         </div>
       </section>
 
-      <section className={styles.medicaid}>
+      <section className={styles.accordian}>
         <div className={styles.insurance_type}>Medicaid </div>
-        <div className={styles.label}>
-          Does this state have a policy in place to use Medicaid funds to cover
-          abortion?
-          <br></br>(Most policies require coverage for &quot;medically
-          necessary&quot; abortion at the discretion of the pregnant person and
-          their medical provider.)
-        </div>
-        <div className={styles.answer}>
-          {medicaid_coverage_provider_patient_decision
-            ? "Yes, this state has a policy in place to use Medicaid funds to pay for abortion."
-            : "No, this state does not have a policy in place for Medicaid funds to cover abortion."}
-        </div>
-        {!medicaid_coverage_provider_patient_decision && (
-          <div className={styles.exceptions}>
-            <h3 className={styles.secondary_label}>
-              <i>EXCEPTIONS TO MEDICAID ABORTION-COVERAGE RESTRICTIONS:</i>
-            </h3>
 
-            <div className={styles.label}>Life-saving exception:</div>
+        <span onClick={() => toggle()}>{showMore ? "-" : "+"}</span>
 
-            <div className={styles.answer}>
-              {medicaid_exception_life
-                ? "The state allows Medicaid insurance to cover abortion when it's necessary to save the pregnant person's life. (This is required by the Hyde amendment.)"
-                : "No life-saving exemption"}
-            </div>
-
-            <div className={styles.label}>Physical health exception:</div>
-            <div className={styles.answer}>{medicaidExceptionHealth()}</div>
-
-            <div className={styles.label}>SERIOUS FETAL ANOMALY EXCEPTION:</div>
-
-            <div className={styles.answer}>{medicaidExceptionFetal()}</div>
-
-            <div className={styles.label}>RAPE OR INCEST Exception:</div>
-            <div className={styles.answer}>
-              {medicaid_exception_rape_or_incest
-                ? "This state allows Medicaid insurance coverage for cases where pregnancy is a result of rape or incest. (This is required by the Hyde amendment)."
-                : "No rape or incest exception."}
-            </div>
+        <div className={getStyle()}>
+          <div className={styles.label}>
+            Does this state have a policy in place to use Medicaid funds to
+            cover abortion?
+            <br></br>(Most policies require coverage for &quot;medically
+            necessary&quot; abortion at the discretion of the pregnant person
+            and their medical provider.)
           </div>
-        )}
+          <div className={styles.answer}>
+            {medicaid_coverage_provider_patient_decision
+              ? "Yes, this state has a policy in place to use Medicaid funds to pay for abortion."
+              : "No, this state does not have a policy in place for Medicaid funds to cover abortion."}
+          </div>
+          {!medicaid_coverage_provider_patient_decision && (
+            <div className={styles.exceptions}>
+              <h3 className={styles.secondary_label}>
+                <i>EXCEPTIONS TO MEDICAID ABORTION-COVERAGE RESTRICTIONS:</i>
+              </h3>
+
+              <div className={styles.label}>Life-saving exception:</div>
+
+              <div className={styles.answer}>
+                {medicaid_exception_life
+                  ? "The state allows Medicaid insurance to cover abortion when it's necessary to save the pregnant person's life. (This is required by the Hyde amendment.)"
+                  : "No life-saving exemption"}
+              </div>
+
+              <div className={styles.label}>Physical health exception:</div>
+              <div className={styles.answer}>{medicaidExceptionHealth()}</div>
+
+              <div className={styles.label}>
+                SERIOUS FETAL ANOMALY EXCEPTION:
+              </div>
+
+              <div className={styles.answer}>{medicaidExceptionFetal()}</div>
+
+              <div className={styles.label}>RAPE OR INCEST Exception:</div>
+              <div className={styles.answer}>
+                {medicaid_exception_rape_or_incest
+                  ? "This state allows Medicaid insurance coverage for cases where pregnancy is a result of rape or incest. (This is required by the Hyde amendment)."
+                  : "No rape or incest exception."}
+              </div>
+            </div>
+          )}
+        </div>
       </section>
     </div>
   );
---
+++ components/PrivateInsurance.js
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+import styles from "../styles/Section.module.css";
+
+const PrivateInsurance = (props) => {
+  const state = props.state;
+
+  const targetData = props.targetData;
+  const [showMore, setShowMore] = useState(false);
+
+  const toggle = () => {
+    return setShowMore(!showMore);
+  };
+
+  const getStyle = () => {
+    if (showMore) return styles.showContent;
+    else return styles.hideContent;
+  };
+
+  // PRIVATE PLANS
+  const requiresCoverage = targetData.requires_coverage
+    ? targetData.requires_coverage
+    : null;
+
+  const privateCoverage = targetData.private_coverage_no_restrictions
+    ? targetData.private_coverage_no_restrictions
+    : null;
+
+  const privateLifeException = targetData.private_exception_life
+    ? targetData.private_exception_life
+    : null;
+
+  const private_exception_fetal = targetData.private_exception_fetal
+    ? targetData.private_exception_fetal
+    : null;
+
+  const private_exception_health = targetData.private_exception_health
+    ? targetData.exception_health
+    : null;
+
+  const privateHealthException = () => {
+    switch (private_exception_health) {
+      case "major bodily function":
+        return "This state allows private insurance coverage when the pregnant person's faces a 'substantial and irreversible impairment of a major bodily function.'";
+      default:
+        return "No exceptions for serious health conditions specified.";
+    }
+  };
+
+  const privateFetalException = () => {
+    switch (private_exception_fetal) {
+      case "Lethal fetal anomaly":
+        return "This state allows private insurance coverage when the fetus has a lethal fetal anomaly.";
+      default:
+        return "No exceptions for lethal fetal anomalies specified.";
+    }
+  };
+
+  const private_exception_rape_or_incest =
+    targetData.private_exception_rape_or_incest
+      ? targetData.private_exception_rape_or_incest
+      : null;
+  return (
+    <div className={styles.data_wrapper}>
+      <section className={styles.accordian}>
+        <div className={styles.insurance_type}>
+          Private health insurance plans
+        </div>
+        {/* IF SHOWMORE IS TRUE, SHOW -, 
+    IF SHOWMORE IS FALSE(DEFAULT), SHOW + */}
+        <span onClick={() => toggle()}>{showMore ? "-" : "+"}</span>
+
+        <div className={getStyle()}>
+          <div className={styles.label}>
+            Does the state require abortion coverage by private health plans
+            regulated by state?
+          </div>
+
+          <div className={styles.answer}>
+            {requiresCoverage
+              ? "Yes, private health plans regulated by the state are required to cover abortions. This regulation does not apply to self-insured plans regulated at the federal versus the state level)."
+              : "No, this state does not have require abortion coverage by private health plans regulated by state."}
+          </div>
+
+          <div className={styles.label}>
+            <b>
+              Can private insurance cover abortions despite not being required
+              to?
+            </b>
+          </div>
+          <div className={styles.answer}>
+            {privateCoverage
+              ? "Private insurance may cover abortion but not necessarily required to."
+              : "This state does not have require coverage and private insurance cannot cover abortion."}
+          </div>
+
+          <div className={styles.exceptions}>
+            <h3 className={styles.secondary_label}>
+              <i>
+                EXCEPTIONS TO PRIVATE HEALTH INSURANCE ABORTION-COVERAGE
+                RESTRICTIONS:
+              </i>
+            </h3>
+
+            <div className={styles.label}>Life-saving exception:</div>
+
+            <div className={styles.answer}>
+              {privateLifeException
+                ? "This state allows private insurance coverage for cases where abortion is necessary to save the pregnant person's life."
+                : "No life-saving exemption"}
+            </div>
+
+            <div className={styles.label}>
+              Serious health condition exception:
+            </div>
+
+            <div className={styles.answer}>{privateHealthException()}</div>
+
+            <div className={styles.label}>Lethal fetal anomaly Exception:</div>
+
+            <div className={styles.answer}>{privateFetalException()}</div>
+
+            <div className={styles.label}>RAPE OR INCEST Exception:</div>
+            <div className={styles.answer}>
+              {private_exception_rape_or_incest
+                ? "This state allows private insurance coverage for cases where pregnancy is a result of rape or incest."
+                : "No rape or incest exception."}
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default PrivateInsurance;
