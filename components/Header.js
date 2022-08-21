1aa9b7973ef673e21c714dddad647b506fa75147
Joann Pan <joannlpan@gmail.com>
2022-08-21 17:13:17 -0400

    Syntax change to correct deployment error.

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import styles from "../styles/Header.module.css";
 import MenuIcon from "@mui/icons-material/Menu";
-import Navbar from "./Navbar";
+import Navbar from "../components/Navbar";
 
 const Header = ({ toggle }) => {
   return (
