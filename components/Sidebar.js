b6788186c156db6cb4ecca62f0b1298316d7d313
Joann Pan <joannlpan@gmail.com>
2022-08-21 14:23:02 -0400

    create sidebar component to hook up to burger icon

---
+++ components/Sidebar.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import styles from "../styles/Sidebar.module.css";
+import CloseIcon from "@mui/icons-material/Close";
+
+const Sidebar = () => {
+  //   console.log("FROM SIDEBAR, SIDEBAR STATE:", props.show);
+  //   const showState = show ? styles.openSidebar : styles.hideSidebar;
+  //   const showSidebar = props.show;
+  return <div>sidebar</div>;
+};
+
+export default Sidebar;
--- pages/states/index.js
+++ pages/states/index.js
@@ -1,18 +1,19 @@
 import Link from "next/link";
 import kebabCase from "../../constants/kebabCase";
 import states from "../../constants/states";
+import styles from "../../styles/States.module.css";
 
 const StatesList = () => {
   let statesFromData = Object.entries(states);
 
   return (
-    <div>
-      <div>
+    <div className={styles.states_container}>
+      <div className={styles.states}>
         {statesFromData.map((state) => {
           const stateNameInKebabCase = kebabCase(state[1]);
 
           return (
-            <div key={state[1]}>
+            <div className={styles.state} key={state[1]}>
               <Link href={"/states/" + stateNameInKebabCase}>
                 <h1>{state[1]}</h1>
               </Link>
