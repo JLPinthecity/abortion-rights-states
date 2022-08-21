06e09c831186771d82f0d728b252e592ae3dc07e
Joann Pan <joannlpan@gmail.com>
2022-08-21 15:24:53 -0400

    add styling to about page

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,29 @@
+import Link from "next/link";
+import styles from "../styles/About.module.css";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className={styles.about_wrapper}>
+      <p>
+        Abortion Rights by State is a tool anyone can use to familiarize
+        themselves with abortion rights in any state.
+      </p>
+
+      <p>
+        State laws are populated programmatically from the{" "}
+        <Link href="https://www.abortionpolicyapi.com">
+          <a target="_blank">Abortion Policy API</a>
+        </Link>
+        . Data from Abortion Policy API is updated at the end of every month.
+      </p>
+
+      <p>
+        Abortion Rights by State aims to provide more clarity about state
+        regulated on gestational limits, private and public insurance covers
+        limits, rules
+      </p>
+    </div>
+  );
 };
 
 export default About;
