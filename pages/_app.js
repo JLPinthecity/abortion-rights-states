5edc71055bd8bd5a84bc5478bddaf20e304ff8fd
Joann Pan <joannlpan@gmail.com>
2022-08-26 15:14:36 -0400

    add script to enable google analytics and next/head to app file

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,33 @@
 import "../styles/fonts.css";
-
+import Head from "next/head";
 import "../styles/globals.css";
 import Layout from "../components/layout";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id="google_analytics" strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
+
+      <Head>
+        <title>Abortion Rights By State</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+
       <Component {...pageProps} />
     </Layout>
   );
